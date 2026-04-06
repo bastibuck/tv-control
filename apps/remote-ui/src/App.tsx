760ebf5fb58f6ee3a014b14b8c066d4d10bea512
Basti Buck <mail@bastibuck.de>
2026-04-06 09:41:16 +0200

    enable button handler

--- apps/remote-ui/src/App.tsx
+++ apps/remote-ui/src/App.tsx
@@ -286,7 +286,7 @@ export function App(): ReactElement {
   }
 
   function handleOpenNetflix(): void {
-    if (!socket || socket.readyState !== WebSocket.OPEN || !extensionConnected) {
+    if (!socket || socket.readyState !== WebSocket.OPEN) {
       return;
     }
 
